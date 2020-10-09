b7c2057d9b4c5c15841f3c6af2f59ae5099d4210
Tiago Moraes <tiagotgob@hotmail.com>
2020-10-09 16:21:53 -0300

    Revert "Splash Screen Activity implemented, first model."
    This reverts commit a8ceae55fffe1e8456fcbb6c3b5a627735a43f16.

--- teste/app/src/main/java/br/com/tgob/mobile2youmovies/activity/SplashActivity.kt
+++ teste/app/src/main/java/br/com/tgob/mobile2youmovies/activity/SplashActivity.kt
@@ -1,23 +1,5 @@
 package br.com.tgob.mobile2youmovies.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import br.com.tgob.mobile2youmovies.R
+class SplashActivity {
 
-class SplashActivity : AppCompatActivity() {
-
-    private val SPLASH_TIME_OUT:Long = 3000
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-
-            finish()
-        }, SPLASH_TIME_OUT)
-    }
 }
