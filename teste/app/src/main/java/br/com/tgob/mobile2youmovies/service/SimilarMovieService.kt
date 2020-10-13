507a0b082af144f57ffcf8e8fca304a3d9811299
Tiago Moraes <tiagotgob@hotmail.com>
2020-10-13 11:18:20 -0300

    found the error in the request, going to work on the adapter now

--- teste/app/src/main/java/br/com/tgob/mobile2youmovies/service/SimilarMovieService.kt
+++ teste/app/src/main/java/br/com/tgob/mobile2youmovies/service/SimilarMovieService.kt
@@ -8,6 +8,6 @@ import retrofit2.http.Query
 
 interface SimilarMovieService {
 
-    @GET("/movie/{movie_id}/similar")
+    @GET("movie/{movie_id}/similar")
     fun getSimilarMovies(@Path("movie_id") id: Int, @Query ("api_key") apiKey: String) : Call<SimilarMoviesEntity>
 }
